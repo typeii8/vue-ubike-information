a2053b3c6992d55a1d0f8d98cf08503e7f84103a
ZoeH <typeii8@gmail.com>
2021-01-15 01:07:53 +0800

    練習 13 - YouBike 臺北市公共自行車即時資訊

--- scripts.js
+++ scripts.js
@@ -1,40 +1,73 @@
 const vm = Vue.createApp({
-    data () {
-      return {
-        ubikeStops: []
+  data() {
+    return {
+      uBikeStops: [],
+      searchName: "",
+      sortedVal: "",
+      sortedType: "",
+    };
+  },
+  created() {
+    // 欄位說明請參照:
+    // http://data.taipei/opendata/datalist/datasetMeta?oid=8ef1626a-892a-4218-8344-f7ac46e1aa48
+
+    // sno：站點代號、 sna：場站名稱(中文)、 tot：場站總停車格、
+    // sbi：場站目前車輛數量、 sarea：場站區域(中文)、 mday：資料更新時間、
+    // lat：緯度、 lng：經度、 ar：地(中文)、 sareaen：場站區域(英文)、
+    // snaen：場站名稱(英文)、 aren：地址(英文)、 bemp：空位數量、 act：全站禁用狀態
+
+    fetch("https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.gz")
+      .then((res) => res.json())
+      .then((json) => {
+        // 將 json 轉陣列後存入 this.ubikeStops
+        const stops = Object.keys(json.retVal).map((key) => json.retVal[key]);
+        this.uBikeStops = stops;
+      });
+  },
+  computed: {
+    filteredStops: function () {
+      const defaultStops = this.uBikeStops.filter((stop) =>
+        stop.sna.includes(this.searchName)
+      );
+      const sortedStops = [...defaultStops];
+      let resultStops;
+
+      if (this.sortedType === "asc") {
+        if (this.sortedVal === "sbi") {
+          resultStops = [...sortedStops.sort((a, b) => a.sbi - b.sbi)];
+        } else {
+          resultStops = [...sortedStops.sort((a, b) => a.tot - b.tot)];
+        }
+      } else if (this.sortedType === "desc") {
+        if (this.sortedVal === "sbi") {
+          resultStops = [...sortedStops.sort((a, b) => b.sbi - a.sbi)];
+        } else {
+          resultStops = [...sortedStops.sort((a, b) => b.tot - a.tot)];
+        }
+      } else {
+        resultStops = [...defaultStops];
       }
+
+      return resultStops;
     },
-    methods: {
-      timeFormat(t){
+  },
+  methods: {
+    timeFormat(t) {
+      var date = [],
+        time = [];
 
-        var date = [], time = [];
+      date.push(t.substr(0, 4));
+      date.push(t.substr(4, 2));
+      date.push(t.substr(6, 2));
+      time.push(t.substr(8, 2));
+      time.push(t.substr(10, 2));
+      time.push(t.substr(12, 2));
 
-        date.push(t.substr(0, 4));
-        date.push(t.substr(4, 2));
-        date.push(t.substr(6, 2));
-        time.push(t.substr(8, 2));
-        time.push(t.substr(10, 2));
-        time.push(t.substr(12, 2));
-
-        return date.join("/") + ' ' + time.join(":");
-      }
+      return date.join("/") + " " + time.join(":");
     },
-    created() {
-
-        // 欄位說明請參照:
-        // http://data.taipei/opendata/datalist/datasetMeta?oid=8ef1626a-892a-4218-8344-f7ac46e1aa48
-
-        // sno：站點代號、 sna：場站名稱(中文)、 tot：場站總停車格、
-        // sbi：場站目前車輛數量、 sarea：場站區域(中文)、 mday：資料更新時間、
-        // lat：緯度、 lng：經度、 ar：地(中文)、 sareaen：場站區域(英文)、
-        // snaen：場站名稱(英文)、 aren：地址(英文)、 bemp：空位數量、 act：全站禁用狀態
-
-        fetch('https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.gz')
-          .then(res => res.json())
-          .then(res => {
-              // 將 json 轉陣列後存入 this.ubikeStops
-              this.ubikeStops = Object.keys(res.retVal).map(key => res.retVal[key]);
-          });
-
-    }
-}).mount('#app');
+    sorted(sortType, sortVal) {
+      this.sortedType = sortType;
+      this.sortedVal = sortVal;
+    },
+  },
+}).mount("#app");
